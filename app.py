39e6f7da9d6c22d2e98f3823f6fc69b85b056210
HijazP <hanifaliramdani98@gmail.com>
2024-07-26 10:46:42 +0700

    nambah konfigurasi host

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predict():
     return render_template('index.html', prediction=result, probability=prob)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
